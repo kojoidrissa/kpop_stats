b3552a2d8678215004b5fce856e68daf8afac19a
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-02-12 21:39:46 -0600

    made kpop_stats_test.py the 'main'file; adjusted it slightly during PyLadies on 2015-02-11; added notes.md to repo

--- kpop_stats_test.py
+++ kpop_stats_test.py
@@ -7,26 +7,26 @@ for video in data['feed']['entry'][0:5]:
     print(video['title']['$t'], "with", '{:,}'.format(view_count), "views") #second 1/2 formats the int with thousands separators
 
 
-#This is the code I used to create the youTube_data.txt file
-'''print()
-print("NEW CODE STARTS HERE!!!")
-print('''This is showing me the results from the "data = response.json"; the type and length of each
-    element in "data['feed']['entry']" ''')
-#this shows me the various data catagories stored in each YouTube Entry listing
-f = open("youTube_data.txt", "a")
+# #This is the code I used to create the youTube_data.txt file
+# '''print()
+# print("NEW CODE STARTS HERE!!!")
+# print('''This is showing me the results from the "data = response.json"; the type and length of each
+#     element in "data['feed']['entry']" ''')
+# #this shows me the various data catagories stored in each YouTube Entry listing
+# f = open("youTube_data.txt", "a")
 
-for i in data['feed']['entry']:
-    # for key in i:
-    #     print (key, )
-    # print (i['yt$statistics']['viewCount'])
-    f.write(str(type(i)) + " " + str(len(i)) + " " + i['title']['$t'] + "\n" + str(i) + "\n" + "\n")
-    print (len(i), i['title']['$t'])
-#Looks like I'm after ['title']['$t'] and ['yt$statistics']
-'''
-f.close()
+# for i in data['feed']['entry']:
+#     # for key in i:
+#     #     print (key, )
+#     # print (i['yt$statistics']['viewCount'])
+#     f.write(str(type(i)) + " " + str(len(i)) + " " + i['title']['$t'] + "\n" + str(i) + "\n" + "\n")
+#     print (len(i), i['title']['$t'])
+# #Looks like I'm after ['title']['$t'] and ['yt$statistics']
+# '''
+# f.close()
 
-for i in data['feed']['entry']:
-    print (type(i), len(i))
-    # print (i['yt$statistics']['viewCount'])
-    print (i['title']['$t'])
-#Looks like I'm after ['title']['$t'] and ['yt$statistics']
+# for i in data['feed']['entry']:
+#     print (type(i), len(i))
+#     # print (i['yt$statistics']['viewCount'])
+#     print (i['title']['$t'])
+# #Looks like I'm after ['title']['$t'] and ['yt$statistics']
---
+++ notes.md
@@ -0,0 +1,3 @@
+#Notes related to this project
+-  [How to find YouTube Video ID of any video](https://www.youtube.com/watch?v=EKyirtVHsK0)
+-  
